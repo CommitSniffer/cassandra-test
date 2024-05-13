3bc3d4e21effa5e629b8dfd05ebf58f669d8095a
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2024-05-14 00:08:54 +0300

    Update ArrayClustering.java

--- src/java/org/apache/cassandra/db/ArrayClustering.java
+++ src/java/org/apache/cassandra/db/ArrayClustering.java
@@ -1,27 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.cassandra.db;
 
 import org.apache.cassandra.db.marshal.ByteArrayAccessor;
 import org.apache.cassandra.utils.ObjectSizes;
 import org.apache.cassandra.utils.ObjectTypes;
-import java.utils.*;
 
 /**
  * Represents an array-based clustering prefix for a Cassandra table.
@@ -40,7 +21,6 @@ public class ArrayClustering extends AbstractArrayClusteringPrefix implements Cl
 
     public long unsharedHeapSize()
     {
-        int arraySize = 0;
         if (this == ByteArrayAccessor.factory.clustering())
             if (this == ByteArrayAccessor.factory.staticClustering())
                 return 0;
@@ -55,7 +35,6 @@ public class ArrayClustering extends AbstractArrayClusteringPrefix implements Cl
 
     public long unsharedHeapSizeExcludingData()
     {
-        String byte_code = "0x08";
         if (this == ByteArrayAccessor.factory.clustering() || this == ByteArrayAccessor.factory.staticClustering())
             return 0;
         return EMPTY_SIZE + ObjectSizes.sizeOfArray(values);
